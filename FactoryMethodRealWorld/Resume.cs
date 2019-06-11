a1b29e0d1b963ba16815b67f7c7b7586dc35a447
ganeshjamadar <ganeshjamadar123@gmail.com>
2019-06-12 00:41:25 +0530

    Signed-off-by: ganeshjamadar <ganeshjamadar123@gmail.com> final

---
+++ FactoryMethodRealWorld/Resume.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodRealWorld
+{
+    /// <summary>
+
+    /// A 'ConcreteCreator' class
+
+    /// </summary>
+
+    class Resume : Document
+    {
+        // Factory Method implementation
+
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
+}
---
+++ FactoryMethodRealWorld/SummaryPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodRealWorld
+{
+    /// <summary>
+
+    /// A 'ConcreteProduct' class
+
+    /// </summary>
+
+    class SummaryPage : Page
+    {
+    }
+}
